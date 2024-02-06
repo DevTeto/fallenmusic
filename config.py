7627eaec4222d2ed204f5d92b15b564e2d8a54b1
Ahmed <157909403+DevTeto@users.noreply.github.com>
2024-02-06 22:00:54 +0200

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "360"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/86a9b4a2223b97848cc1d.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/86a9b4a2223b97848cc1d.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/a7a74c92af4aff94d158f.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/a7a74c92af4aff94d158f.jpg")
 
 SESSION = getenv("SESSION", None)
 
@@ -24,4 +24,4 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/wx_pm")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6945048358").split()))
 
 
-FAILED = "https://telegra.ph/file/86a9b4a2223b97848cc1d.jpg"
+FAILED = "https://telegra.ph/file/a7a74c92af4aff94d158f.jpg"
